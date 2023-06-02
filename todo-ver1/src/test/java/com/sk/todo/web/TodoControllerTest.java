dde7f5ebffbd958f1715106b21ee04c6e7021ea9
DESKTOP-RCJOS5S\sangkook <sixstar6@naver.com>
2023-06-03 06:00:23 +0900

    convert to thymeleaf

--- todo-ver1/src/test/java/com/sk/todo/web/TodoControllerTest.java
+++ todo-ver1/src/test/java/com/sk/todo/web/TodoControllerTest.java
@@ -2,14 +2,25 @@ package com.sk.todo.web;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
 public class TodoControllerTest {
 
-    TodoController todoController = new TodoController();
+    @Autowired
+    MockMvc mockMvc;
 
     @Test
-    void todoControllerTest(){
-        final var todo = todoController.todo();
-        Assertions.assertThat(todo).isEqualTo("todo");
+    void todoControllerTest() throws Exception {
+        mockMvc.perform(get("/todo"))
+                .andExpect(status().isOk());
     }
 }
