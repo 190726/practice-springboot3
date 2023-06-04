1b06190ee81868c8b2fe7c6a5c7dfb4a53050301
DESKTOP-RCJOS5S\sangkook <sixstar6@naver.com>
2023-06-05 05:39:42 +0900

    add 'ReqeustMapping' on TodoController Class

--- todo-ver1/src/main/java/com/sk/todo/web/TodoController.java
+++ todo-ver1/src/main/java/com/sk/todo/web/TodoController.java
@@ -3,10 +3,7 @@ package com.sk.todo.web;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -14,12 +11,13 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/todo")
 public class TodoController {
 
     private final TodoService todoService;
     private final TodoMapper mapper;
 
-    @PostMapping("/todo")
+    @PostMapping
     public ResponseEntity<ToDo> todo(TodoRequest request) {
         var saveTodo = todoService.save(mapper.apply(request));
         //TODO, MDN Web Docs : Created 상태 코드는 새로 생성된 자원의 주소를 함께 반환해야 한다.
@@ -29,12 +27,12 @@ public class TodoController {
         return ResponseEntity.created(uri).body(saveTodo);
     }
 
-    @GetMapping("/todo")
+    @GetMapping
     public List<ToDo> all(){
         return todoService.retrieveAll();
     }
 
-    @GetMapping("/todo/{id}")
+    @GetMapping("/{id}")
     public ToDo retrieve(@PathVariable  long id){
         return todoService.findOne(id);
     }
